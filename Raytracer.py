023f95c6b9b70c26911c0f446cb9f8d8a0da6b23
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2022-09-23 11:13:47 -0600

    entrega RT2: Opaque, Reflections & Refractions:

--- Raytracer.py
+++ Raytracer.py
@@ -4,10 +4,20 @@ from figures import *
 from lights import *
 
 
-width = 500
-height = 500
-
+width = 1600
+height = 800
+centrox = 0
+centroy = -1
 # Materiales
+
+material_opaco1 = Material(diffuse = (0.0546, 0.472, 0.0664), spec=32, matType=OPAQUE)
+material_opaco2 = Material(diffuse = (0.994, 0.186, 0.233), spec=32, matType=OPAQUE)
+
+material_reflectivo1 = Material(diffuse = (0.384, 0.501, 0.605), spec=32, matType=REFLECTIVE)
+material_reflectivo2 =Material(diffuse = (0.9, 0.9, 0.9), spec = 64, matType = REFLECTIVE)
+
+material_transparente1 = Material(diffuse = (0.805, 0.513, 0.303), spec=32, matType=TRANSPARENT)
+material_transparente2 = Material(diffuse = (0.804, 0.707, 0.973), spec=32, matType=TRANSPARENT)
 
 brick = Material(diffuse = (0.8, 0.3, 0.3), spec = 16)
 stone = Material(diffuse = (0.4, 0.4, 0.4), spec = 8)
@@ -20,19 +30,19 @@ yellowMirror = Material(diffuse = (0.9, 0.9, 0.2), spec = 64, matType = REFLECTI
 
 rtx = Raytracer(width, height)
 
-rtx.envMap = Texture("parkingLot.bmp")
+rtx.envMap = Texture("casita.bmp")
 
 rtx.lights.append( AmbientLight(intensity = 0.1 ))
 rtx.lights.append( DirectionalLight(direction = (-1,-1,-1), intensity = 0.8 ))
 #rtx.lights.append( PointLight(point = (0,0,0)))
 
-rtx.scene.append( Sphere(V3(0,0,-10), 1, earth)  )
+rtx.scene.append( Sphere(V3(-3+centrox,3+centroy,-10), 1, material_opaco1)  )
+rtx.scene.append( Sphere(V3(0+centrox,3+centroy,-10), 1, material_opaco2)  )
+rtx.scene.append( Sphere(V3(3+centrox,3+centroy,-10), 1, material_reflectivo1)  )
 
-# rtx.scene.append( Sphere(V3(3,0,-10), 1, brick)  )
-# rtx.scene.append( Sphere(V3(0,3,-10), 1, stone)  )
-
-# rtx.scene.append( Sphere(V3(-3,0,-10),1, blueMirror)  )
-# rtx.scene.append( Sphere(V3(0,-3,-10), 1, yellowMirror)  )
+rtx.scene.append( Sphere(V3(-3+centrox,0+centroy,-10),1, material_reflectivo2)  )
+rtx.scene.append( Sphere(V3(0+centrox,0+centroy,-10), 1, material_transparente1)  )
+rtx.scene.append( Sphere(V3(3+centrox,0+centroy,-10), 1, material_transparente2)  ) 
 
 
 rtx.glRender()
